c097504c28a95a59d4e2c75e58a99cad50e4007a
Lucas-E-Rocha <lucaskinnur@hotmail.com>
2026-04-14 16:39:15 -0300

    Exercicios 5
    parte 2

---
+++ UdemyJavaCompletoPOO/src/exercicios5/Ex2/App/Program.java
@@ -0,0 +1,31 @@
+package exercicios5.Ex2.App;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import exercicios5.Ex2.Entities.Employee;
+
+public class Program {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+
+		Employee employee = new Employee();
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Name: ");
+		employee.name = sc.nextLine();
+		System.out.print("Gross salary: ");
+		employee.grossSalary = sc.nextDouble();
+		System.out.print("Tax: ");
+		employee.tax = sc.nextDouble();
+		
+		System.out.printf("Employee: "+employee.name+", $ %.2f%n",employee.NetSalary());
+		
+		System.out.print("Which percentage to increase salary? ");
+		employee.IncreaseSalary(sc.nextDouble());
+		
+		System.out.printf("Updated data: "+employee.name+", $ %.2f%n",employee.NetSalary());
+
+		sc.close();
+	}
+}
