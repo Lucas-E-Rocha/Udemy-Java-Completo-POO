6e9b87b0804eb39f890e081025d14b55f3787ede
Lucas-E-Rocha <lucaskinnur@hotmail.com>
2026-04-14 05:28:43 -0300

    Exercicios 5
    Ex1

---
+++ UdemyJavaCompletoPOO/src/exercicios5/Ex1/app/Programa.java
@@ -0,0 +1,27 @@
+package exercicios5.Ex1.app;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import exercicios5.Ex1.entidades.Rectangle;
+
+public class Programa {
+	public static void main(String[] args) {
+		Rectangle r = new Rectangle();
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		
+		System.out.println("Enter rectangle width and height:");
+		r.width = sc.nextDouble();
+		r.height = sc.nextDouble();
+
+		System.out.printf("Area: %.2f%n"
+						+ "Perimeter: %.2f%n"
+						+ "Diagonal: %.2f%n"
+						,r.Area()
+						,r.Perimeter()
+						,r.Diagonal());
+		
+		sc.close();
+	}
+}
