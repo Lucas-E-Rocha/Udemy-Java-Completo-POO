3eff08871e6956480e64306d38307ee92f55a4d1
Lucas-E-Rocha <lucaskinnur@hotmail.com>
2026-04-29 19:10:56 -0300

    JDBC excercicios 1+2

---
+++ Bonus/src/db/exercicios1/Program.java
@@ -0,0 +1,12 @@
+package db.exercicios1;
+
+import java.sql.Connection;
+
+import db.DB;
+
+public class Program {
+	public static void main(String[] args) {
+		Connection conn = DB.getConnection();
+		DB.closeConnection();
+	}
+}
