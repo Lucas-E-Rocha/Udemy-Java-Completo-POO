6c1c7955848a50ca32dc3eb53cd46f89d29f683e
Lucas-E-Rocha <lucaskinnur@hotmail.com>
2026-05-05 03:15:55 -0300

    Bonus Projeto Spring Boot e JPA / Hibernate - Parte 4
    Camada de serviço, registro de componentes

---
+++ Bonus/SpringJPA/src/main/java/com/udemyJavaPoo/SpringJPA/services/UserService.java
@@ -0,0 +1,26 @@
+package com.udemyJavaPoo.SpringJPA.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.udemyJavaPoo.SpringJPA.entities.User;
+import com.udemyJavaPoo.SpringJPA.respositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
