c73301b2d2cf8aac41ca3ffd7e5720a6eebbbbb1
Lucas-E-Rocha <lucaskinnur@hotmail.com>
2026-05-11 19:47:43 -0300

    Bonus MongoDB
    Criação do projeto

---
+++ Bonus/workshopmongo/src/test/java/com/udemyJavaPooSpringMongoDB/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemyJavaPooSpringMongoDB.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
