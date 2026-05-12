5bf481ddae5005f80de807580f70239884f3d27f
Lucas-E-Rocha <lucaskinnur@hotmail.com>
2026-05-12 00:42:29 -0300

    Bonus MongoDB - Parte 5
    Criando entity Post com User aninhado, Projeção dos dados do autor com DTO, Referenciando os posts do usuário, Endpoint para retornar os posts de um usuário, Obtendo um post por id, Acrescentando comentários aos posts

---
+++ Bonus/workshopmongo/src/main/java/com/udemyJavaPooSpringMongo/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.udemyJavaPooSpringMongo.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.udemyJavaPooSpringMongo.workshopmongo.domain.Post;
+import com.udemyJavaPooSpringMongo.workshopmongo.repository.PostRepository;
+import com.udemyJavaPooSpringMongo.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
