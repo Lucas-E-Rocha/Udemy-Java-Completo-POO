d0eba2ecf0ceedae3649be3f79bfffb905fbcf5d
Lucas-E-Rocha <lucaskinnur@hotmail.com>
2026-05-12 01:40:30 -0300

    Bonus MongoDB - Parte 7 (Final)
    Consulta com vários critérios

--- Bonus/workshopmongo/src/main/java/com/udemyJavaPooSpringMongo/workshopmongo/services/PostService.java
+++ Bonus/workshopmongo/src/main/java/com/udemyJavaPooSpringMongo/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.udemyJavaPooSpringMongo.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,9 +22,14 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
-	public List<Post> findByTitle(String text){
+	public List<Post> findByTitle(String text) {
 		return repo.searchTitle(text);
 		// Método usando query methods
-		//return repo.findByTitleContainingIgnoreCase(text);
+		// return repo.findByTitleContainingIgnoreCase(text);
+	}
+
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 }
