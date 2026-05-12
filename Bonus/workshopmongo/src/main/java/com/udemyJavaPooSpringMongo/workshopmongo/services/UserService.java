26a2de0030529c8b0c56804b0c3e58983d032984
Lucas-E-Rocha <lucaskinnur@hotmail.com>
2026-05-11 23:43:58 -0300

    Bonus MongoDB - Parte 4
    Inserção de usuário com POST, Deleção de usuário com DELETE, Atualização de usuário com PUT

--- Bonus/workshopmongo/src/main/java/com/udemyJavaPooSpringMongo/workshopmongo/services/UserService.java
+++ Bonus/workshopmongo/src/main/java/com/udemyJavaPooSpringMongo/workshopmongo/services/UserService.java
@@ -7,21 +7,47 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.udemyJavaPooSpringMongo.workshopmongo.domain.User;
+import com.udemyJavaPooSpringMongo.workshopmongo.dto.UserDTO;
 import com.udemyJavaPooSpringMongo.workshopmongo.repository.UserRepository;
 import com.udemyJavaPooSpringMongo.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+	}
+	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj,obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
 }
